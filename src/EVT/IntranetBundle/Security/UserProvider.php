ed281c898ded683b4e1b09fe77f603eff642d3ec
Quique Torras <etorras@gmail.com>
2014-02-20 12:37:49 +0100

    [#AE-6] - User provider

---
+++ src/EVT/IntranetBundle/Security/UserProvider.php
@@ -0,0 +1,46 @@
+<?php
+namespace EVT\IntranetBundle\Security;
+
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\User;
+
+class UserProvider implements UserProviderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        // make a call to your webservice here
+        $userData = 'ss';
+        // pretend it returns an array on success, false if there is no user
+
+        if ($userData) {
+            $password = 'userpass';
+            $roles = ['ROLE_USER'];
+
+            $obj = new User($username, $password, $roles);
+            return $obj;
+        }
+
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class)
+    {
+        return $class === 'Symfony\Component\Security\Core\User\User';
+    }
+}
