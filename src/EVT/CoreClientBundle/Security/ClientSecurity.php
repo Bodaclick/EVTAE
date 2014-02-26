5456a183870679c1c0b049752e7781e74643d76f
Marco Ferrari <marco.ferrari@bodaclick.com>
2014-02-26 09:54:37 +0100

    [#AE-37] - SecurizeUrl implemented

--- src/EVT/CoreClientBundle/Security/ClientSecurity.php
+++ src/EVT/CoreClientBundle/Security/ClientSecurity.php
@@ -3,6 +3,8 @@
 namespace EVT\CoreClientBundle\Security;
 
 use EVT\CoreClientBundle\Client\Response;
+use Guzzle\Http\Message\Response as GuzzleResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * ClientSecurity
@@ -13,14 +15,43 @@ use EVT\CoreClientBundle\Client\Response;
  */
 class ClientSecurity
 {
+
+    protected $container;
+    protected $context;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function securizeUrl($url)
     {
+        $this->context = $this->container->get('security.context');
         $securizedUrl = $url;
+        $username = $this->context->getToken()->getUsername();
+
+        if (false !== strpos($securizedUrl, '?')) {
+            $securizedUrl .= '&canView='. $username;
+        } else {
+            $securizedUrl .= '?canView='. $username;
+        }
+
         return $securizedUrl;
     }
 
-    public function securizeResponse($response)
+    public function securizeResponse(GuzzleResponse $response)
     {
-        return new Response('200', $response->json());
+        // $this->context = $this->container->get('security.context');
+        // $username = $this->context->getToken()->getUsername();
+        // $roles = $this->context->getToken()->getRoles();
+        // $unsetters = $this->getUnsetters($roles);
+        // $respArray = $response->json();
+
+        return new Response($response->getStatusCode(), $response->json());
+    }
+
+    private function getUnsetters($roles)
+    {
+        return "obj > removeMe";
     }
 }
---
+++ src/EVT/CoreClientBundle/Tests/Security/ClientSecurityTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace EVT\CoreClientBundle\Tests\Security;
+
+use Guzzle\Http\Message\Response;
+use EVT\CoreClientBundle\Security\ClientSecurity;
+
+/**
+ * ClientSecurityTest
+ *
+ * @author    Marco Ferrari <marco.ferrari@bodaclick.com>
+ * @copyright 2014 Bodaclick S.A.
+ */
+class ClientSecurityTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSecurizeUrl()
+    {
+        $sClient = new ClientSecurity($this->getContainer());
+
+        $this->assertEquals('?canView=test@test.com', $sClient->securizeUrl(''));
+        $this->assertEquals('/api/test?canView=test@test.com', $sClient->securizeUrl('/api/test'));
+    }
+
+    public function testSecurizeResponse()
+    {
+        $sClient = new ClientSecurity($this->getContainer());
+
+        $response = new Response(200, null, '{"data":"test"}');
+        $sResponse = $sClient->securizeResponse($response);
+        $this->assertEquals(200, $sResponse->getStatusCode());
+        $this->assertEquals(['data'=>'test'], $sResponse->getBody());
+
+        $response = new Response(404, null, '{}');
+        $sResponse = $sClient->securizeResponse($response);
+        $this->assertEquals(404, $sResponse->getStatusCode());
+        $this->assertEquals([], $sResponse->getBody());
+
+        $response = new Response(404, null, '');
+        $sResponse = $sClient->securizeResponse($response);
+        $this->assertEquals(404, $sResponse->getStatusCode());
+        $this->assertEquals([], $sResponse->getBody());
+    }
+
+    public function testSecurizeResponseRemoveFields()
+    {
+        $sClient = new ClientSecurity($this->getContainer());
+
+        $response = new Response(200, null, '{"data":"test", "obj":{ "persist":"noRemove", "removeMe":"toRemove" }}');
+        $sResponse = $sClient->securizeResponse($response);
+        $this->assertEquals(200, $sResponse->getStatusCode());
+        $this->markTestIncomplete('Not to implements yet');
+        $this->assertEquals(['data'=>'test', 'obj' => ['persist' => 'noRemove']], $sResponse->getBody());
+    }
+
+    private function getContainer()
+    {
+
+
+        $secTokenMock = $this->getMockBuilder('Symfony\Component\Security\Core\Authentication\Token\TokenInterface')
+            ->disableOriginalConstructor()->getMock();
+        $secTokenMock->expects($this->any())->method('getUsername')->will($this->returnValue('test@test.com'));
+        $secTokenMock->expects($this->any())->method('getRoles')
+            ->will($this->returnValue(['ROLE_TEST', 'ROLE_USER']));
+
+        $secContainerMock = $this->getMockBuilder('Symfony\Component\Security\Core\SecurityContextInterface')
+            ->disableOriginalConstructor()->getMock();
+        $secContainerMock->expects($this->any())->method('getToken')->will($this->returnValue($secTokenMock));
+
+        $containerMock = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerInterface')
+            ->disableOriginalConstructor()->getMock();
+        $containerMock->expects($this->any())->method('get')->will($this->returnValue($secContainerMock));
+
+        return $containerMock;
+    }
+}
