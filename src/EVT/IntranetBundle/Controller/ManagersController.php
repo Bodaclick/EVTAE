dde9ee77c2cdb4476216ca5dfe99130745eb5052
alvaro prudencio <aprudencio@bodaclick.com>
2014-04-08 13:39:37 +0200

    [#AE-19] - Cecilia can view a list of manager users

---
+++ src/EVT/IntranetBundle/Controller/ManagersController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EVT\IntranetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class ManagersController extends Controller
+{
+    /**
+     * @Route("/managers")
+     */
+    public function listAction(Request $request)
+    {
+        $managersResponse = $this->container->get('evt.core.client')
+            ->get('/api/managers?canView='.$request->get('canView').'&page='.$request->get('page', 1));
+
+        if (404 == $managersResponse->getStatusCode() && $request->query->get('page', 1)>1) {
+            throw new NotFoundHttpException();
+        }
+
+        $managers = [];
+        if (isset($managersResponse->getBody()['items'])) {
+            $managers = $managersResponse->getBody()['items'];
+        }
+
+        $pagination = $managersResponse->getBody()['pagination'];
+
+        $content = $this->renderView(
+            'EVTIntranetBundle:Lists:managers.html.twig',
+            [
+                "managers" => $managers,
+                "pagination" => $pagination,
+                "routeController" => "evt_intranet_managers_list"
+            ]
+        );
+        return new Response($content);
+    }
+}
