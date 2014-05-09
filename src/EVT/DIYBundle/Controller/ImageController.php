4ae4aae6f0b42f86f068047c1e9b264cecd52e4b
Quique Torras <etorras@gmail.com>
2014-05-09 14:49:56 +0200

    [#AE-76] - File Uploader

---
+++ src/EVT/DIYBundle/Controller/ImageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EVT\DIYBundle\Controller;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\View\View as FosView;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * ImageController
+ *
+ * @author    Quique Torras <etorras@bodaclick.com>
+ *
+ * @copyright 2014 Bodaclick S.A.
+ */
+class ImageController extends Controller
+{
+    public function postImageAction(Request $request)
+    {
+        $image = $request->files->get('image');
+
+        if (!$image instanceof UploadedFile) {
+            throw new BadRequestHttpException('No file');
+        }
+
+        $url = $this->container->get('evt.diy.uploader')->upload($image);
+
+        $view = FosView::create();
+        $view->setStatusCode(Codes::HTTP_CREATED)
+            ->setData(
+                ['url' =>  $url]
+            );
+
+        return $view;
+    }
+}
