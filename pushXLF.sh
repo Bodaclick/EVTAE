c65f97d3b95d0508712484b968aa7495132abfd4
Marco Ferrari <marco.ferrari@bodaclick.com>
2014-05-29 13:01:54 +0200

    [] - Changes in mailing

--- pushXLF.sh
+++ pushXLF.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+app/console translation:update --output-format=xlf --force en EVTEAEBundle
+
+app/console translation:update --output-format=xlf --force en EVTIntranetBundle
+
 cp src/EVT/EAEBundle/Resources/translations/messages.en.xlf translations/bdk-i18n.evtae/en.xlf
 
 cp src/EVT/IntranetBundle/Resources/translations/messages.en.xlf translations/bdk-i18n.evtintranet/en.xlf
